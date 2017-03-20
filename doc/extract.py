92252511471a0c5ae6d8cdcbfb3943fb33a3adf3
Rodrigo Alejandro Melo <rmelo@inti.gob.ar>
2017-03-20 12:16:46 -0300

    Added several characters sustitution in extract.py for Bison code generation

--- doc/extract.py
+++ doc/extract.py
@@ -22,9 +22,45 @@ for word in reserved:
 print ("\n\n")
 
 print ("* Bison code:")
-text = re.sub(r"\n ", "", text)                 # Deleting line jumps
+text = re.sub(r"\n ", " ", text)                # Deleting line jumps
 text = re.sub(r"\n{2,}", "\n\n" , text)         # Delete multiple line jumps
 text = re.sub(r" +", " " , text)                # Delete multiple spaces
 text = re.sub(r"\*[a-z_]*\*", "" , text)        # Delete text between *
 text = re.sub(r"([A-Z]{2,})", r"TOK_\1" , text) # Delete text between *
+
+# Add "" to characters which are 'part of the syntax'
+text = re.sub(r"\[ \[ type_mark", r'"[" [ type_mark' , text)
+text = re.sub(r"type_mark \] \]", r'type_mark ] "]"' , text)
+text = re.sub(r";", r'";"' , text)
+text = re.sub(r" # ", r' "#" ' , text)
+text = re.sub(r" : ", r' ":" ' , text)
+text = re.sub(r" :\n", r' ":"\n' , text)
+text = re.sub(r" \+ ", r' "+" ' , text)
+text = re.sub(r" - ", r' "-" ' , text)
+text = re.sub(r" \* ", r' "*" ' , text)
+text = re.sub(r" / ", r' "/" ' , text)
+text = re.sub(r" \&", r' "&"' , text)
+text = re.sub(r" /=", r' "/="' , text)
+text = re.sub(r" <=", r' "<="' , text)
+text = re.sub(r" >=", r' ">="' , text)
+text = re.sub(r" =>", r' "=>"' , text)
+text = re.sub(r" <>", r' "<>"' , text)
+text = re.sub(r" :=", r' ":="' , text)
+text = re.sub(r" = ", r' "=" ' , text)
+text = re.sub(r" < ", r' "<" ' , text)
+text = re.sub(r" > ", r' ">" ' , text)
+text = re.sub(r" \(", r' "("' , text)
+text = re.sub(r" \)", r' ")"' , text)
+text = re.sub(r" \. ", r' "." ' , text)
+text = re.sub(r" , ", r' "," ' , text)
+text = re.sub(r" \\ ", r' "\" ' , text)
+text = re.sub(r" @ ", r' "@" ' , text)
+text = re.sub(r" : ", r' ":" ' , text)
+text = re.sub(r" B ", r' "B" ' , text)
+text = re.sub(r" O ", r' "O" ' , text)
+text = re.sub(r" X\n", r' "X"\n' , text)
+
+text = re.sub(r" \| ", r"\n     | " , text)
+text = re.sub(r"::=", r":" , text)
+
 print (text)
