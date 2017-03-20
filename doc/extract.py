7b2ca3c05c20673c43deb7825c5d3d35865754a2
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2017-03-20 00:17:11 -0300

    Added code generation (incomplete) for Bison in extract.py

--- doc/extract.py
+++ doc/extract.py
@@ -13,9 +13,18 @@ reserved.sort()
 print ("* Reserved words (%d):" % len(reserved))
 print (", ".join(reserved))
 
-print ("")
+print ("\n\n")
 
 print ("* Reserved Words for Flex code")
 for word in reserved:
     print ("%-30s{ return TOK_%-14s }" % ("\"" + word.lower() + "\"", word + ";") )
 
+print ("\n\n")
+
+print ("* Bison code:")
+text = re.sub(r"\n ", "", text)                 # Deleting line jumps
+text = re.sub(r"\n{2,}", "\n\n" , text)         # Delete multiple line jumps
+text = re.sub(r" +", " " , text)                # Delete multiple spaces
+text = re.sub(r"\*[a-z_]*\*", "" , text)        # Delete text between *
+text = re.sub(r"([A-Z]{2,})", r"TOK_\1" , text) # Delete text between *
+print (text)
